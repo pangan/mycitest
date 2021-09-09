c78a3389d891f701d6278e67bdb36baa1ba2ce2a
pangan <pangan@gmail.com>
2021-09-09 13:36:29 +0100

    testing connectors

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VENV_DIR=.virtualenvs
+VENV_DIR=~
 
 echo "Making Virtual env"
 python3 -m venv $VENV_DIR/connectors
 source $VENV/connectors/bin/activate
-# python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install --upgrade pip setuptools wheel
 
-python3 -m pip install --upgrade -r requirements.txt
+cat requirements.txt| xargs -n 1 python3 -m pip install --upgrade -r requirements.txt
