d3b1f1520550269989dce9e7faec390334ba8212
pangan <pangan@gmail.com>
2021-09-09 10:44:09 +0100

    testing connectors

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ python3 -m venv $VENV_DIR/connectors
 source $VENV/connectors/bin/activate
 python3 -m pip install --upgrade pip setuptools wheel
 
-python3 -m pip install -- upgrade -r requirements.txt
+python3 -m pip install --upgrade -r requirements.txt
